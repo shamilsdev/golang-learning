b27ac45eb9364d11092bf1cfdb48a22335b7f3db
s.sarybashev <s.sarybashev@ipc.kg>
2019-10-15 17:17:43 +0600

    Deleted: priter.go file Modified: main.go file

--- Chapter_1/main.go
+++ Chapter_1/main.go
@@ -1,5 +1,26 @@
 package main
 
+import "fmt"
+
+// PrintLength is func for printing length
+func PrintLength(str string) {
+	l := len(str)
+
+	printLine(l)
+
+	fmt.Println(str)
+	fmt.Println("Length:", l)
+
+	printLine(l)
+}
+
+func printLine(length int) {
+	for i := 0; i < length; i++ {
+		fmt.Print("-")
+	}
+	fmt.Println()
+}
+
 func main() {
 	PrintLength("Hello goooooooooooooooooooooooooooooooooooooooolang!")
 }
