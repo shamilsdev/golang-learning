86c5e0951bd4e20ce65761d769b53d43e1d552e8
s.sarybashev <s.sarybashev@ipc.kg>
2019-10-15 17:48:40 +0600

    Chapter_1 modified

---
+++ Chapter_1/variables/main.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+	var (
+		str string
+		in  int
+		b   bool
+	)
+
+	str = "string"
+	in = 100
+	b = true
+
+	str2 := "string2"
+	in2 := 200
+	b2 := false
+
+	fmt.Println(str)
+	fmt.Println(in)
+	fmt.Println(b)
+
+	fmt.Println(str2)
+	fmt.Println(in2)
+	fmt.Println(b2)
+}
