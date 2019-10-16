0a7a99045211f74ffd083a7d3d5c7dc6810a80fd
s.sarybashev <s.sarybashev@ipc.kg>
2019-10-16 15:51:05 +0600

    added changes

--- Chapter_1/12_map/main.go
+++ Chapter_1/12_map/main.go
@@ -3,12 +3,21 @@ package main
 import "fmt"
 
 func main() {
-	var people = map[string]int{"Tom": 1, "Bob": 2, "Alan": 3, "Sam": 4}
-	fmt.Println(people)
+	var people = map[string]int{"Mike": 1, "Rob": 2, "Pike": 3}
 
-	fmt.Println(people["Sam"])
-
-	if val, ok := people["Smith"]; ok {
+	if val, ok := people["Mikes"]; ok {
 		fmt.Println(ok, val)
 	}
+
+	var users = make(map[int]string)
+	users[1] = "Sam"
+	users[2] = "Smith"
+	users[3] = "Jonas"
+	fmt.Println(users)
+
+	users[4] = "Nick"
+
+	delete(users, 3)
+	fmt.Println(users)
+
 }
