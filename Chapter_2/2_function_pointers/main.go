ec53067dc1d46bce1ab3575aaf4f8e3c20cb64d7
s.sarybashev <s.sarybashev@ipc.kg>
2019-10-18 10:15:01 +0600

    updated pointers

--- Chapter_2/2_function_pointers/main.go
+++ Chapter_2/2_function_pointers/main.go
@@ -10,8 +10,38 @@ func main() {
 	changeParam(&x)
 
 	fmt.Println(x)
+
+	changeParam2(&x)
+
+	fmt.Println(x)
+
+	fmt.Println("------------------------------")
+
+	val := new(int)
+
+	fmt.Println(*val)
+
+	fmt.Println("------------------------------")
+
+	createPointer(10)
+
 }
 
 func changeParam(p *int) {
 	*p = *p * *p
+}
+
+func changeParam2(p *int) {
+	*p = *p * *p
+}
+
+func createPointer(x int) {
+	fmt.Println(x)
+
+	val := new(int)
+	fmt.Println(val)
+
+	*val = x
+	fmt.Println(*val)
+	fmt.Println(&val)
 }
